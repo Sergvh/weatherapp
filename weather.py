3fb66c47330029b37fe174686a365f7aec33b670
”Serhii <sergvh@gmail.com>
2018-11-13 22:11:11 +0200

    intermediate commit

--- weather.py
+++ weather.py
@@ -1,1 +1,12 @@
-print("Hello I am Here!!!!")
+#!usr/bin/python3.6
+
+from urllib.request import urlopen, Request
+
+ACCU_URL = "https://www.accuweather.com/uk/ua/lviv/324561/weather-forecast/324561"
+
+headers = {'User-Agent' : 'Mozilla/5.0 (X11; Fedora; Linux x86_64)'}
+
+accu_request = Request(ACCU_URL, headers=headers)
+accu_page = urlopen(accu_request).read()
+
+print(accu_page)
