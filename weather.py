cbeafab51e7b14545912399fb51a5581260754cc
”Serhii <sergvh@gmail.com>
2018-11-18 19:18:45 +0200

    Wheather from accuweather

--- weather.py
+++ weather.py
@@ -1,12 +1,40 @@
 #!usr/bin/python3.6
+import html
 
 from urllib.request import urlopen, Request
 
 ACCU_URL = "https://www.accuweather.com/uk/ua/lviv/324561/weather-forecast/324561"
 
-headers = {'User-Agent' : 'Mozilla/5.0 (X11; Fedora; Linux x86_64)'}
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64)'}
 
 accu_request = Request(ACCU_URL, headers=headers)
 accu_page = urlopen(accu_request).read()
+accu_page = accu_page.decode('utf-8')
 
-print(accu_page)
+ACCU_TEMP_TAG = '<span class="large-temp">'
+accu_temp_tag_size = len(ACCU_TEMP_TAG)
+accu_temp_tag_index = accu_page.find(ACCU_TEMP_TAG)
+accu_temp_value_start = accu_temp_tag_index + accu_temp_tag_size
+accu_temp = ''
+
+for char in accu_page[accu_temp_value_start:]:
+    if char != '<':
+        accu_temp += char
+    else:
+        break
+
+print('Accuweather: \n')
+print(f'Temperature: {html.unescape(accu_temp)}\n')
+
+ACCU_COND_TAG = '<span class="cond">'
+accu_cond_tag_size = len(ACCU_COND_TAG)
+accu_cond_tag_index = accu_page.find(ACCU_COND_TAG)
+accu_cond_value_start = accu_cond_tag_index + accu_cond_tag_size
+
+accu_temp = ''
+for char in accu_page[accu_cond_value_start:]:
+    if char != '<':
+        accu_temp += char
+    else:
+        break
+print(f'Condition: {accu_temp}\n')
