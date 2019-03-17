ab453c1b4014eb179dc176958d7752ffe663e53a
”Serhii <sergvh@gmail.com>
2019-03-17 17:03:39 +0200

    1

--- 1.py
+++
@@ -1,9 +0,0 @@
-def get_number():
-    try:
-        return int(input('Enter number:'))
-    except ValueError:
-        print('Bad number!!')
-    finally:
-        return int(input('Enter number:'))
-
-get_number()
